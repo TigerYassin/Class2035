b2fa155dff19a7c9af243de4cda305c7d8fd58fc
Yassin Alsahlani <yasnarutoa@gmail.com>
2018-10-09 14:34:50 -0700

    minesweep assembly started

---
+++ C/P1-1-64bit/P1.1.c
@@ -0,0 +1,19 @@
+/*    This program solves a minesweeper game.
+
+PLEASE FILL IN THESE FIELDS:
+Your Name:
+Date:
+ */
+
+#include "minefield.h"
+
+void solver(int mn) {
+  // replace the following 4 dummy instructions with your code
+	guess(2, 3);
+	display_field_discovered_so_far();
+	open(1,0);
+	display_field_discovered_so_far();
+	flag(4, 5);
+	display_field_discovered_so_far();
+}
+
