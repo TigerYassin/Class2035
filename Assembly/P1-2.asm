159f1e59e89f03e0f54d4a9c7864840461566a21
Yassin Alsahlani <yasnarutoa@gmail.com>
2018-10-11 21:55:16 -0700

    able to add box to memory

--- Assembly/P1-2.asm
+++ Assembly/P1-2.asm
@@ -13,175 +13,149 @@ MineSweep: swi   567	   	   # Bury mines (returns # buried in $1)
   
 
 
-test:      addi  $2, $0, 13     # This is the cell that would be opened (input is the cell you want opened)      
-           addi  $3, $0, -1     # (the input is for the command you want, -1 is Guess, 0 is to open)
+test:      addi  $2, $0, 13     # This is the cell that would be opened (input is the cell you want opened)   
+
+           addi $9, $0, 4        #mulitplier for index    
+           addi $3, $0, -1
            swi   568            # returns result in $4 (-1: mine; 0-8: count)
            beq $4, $0, OpenAr
            j GuessAr
            j END
 
 
-###The PROBLEM : 
+###The PROBLEM : Add the number on the box to the memory 
 
 ###OUR SOLUTION: 
-            
-            
-GuessAr:    j RightCen
-            addi $9, $0, 4
 
-RightCen:   addi $8, $0, 2
-            addi $2, $2, 1
+# I want to be able to guess open a box, 
+    #If that guess gives me a zero, then call OpenAr
+    #If that guess gives anything else, then I want to guess open another box
+          #After I open that surrounding box, I want to see if it is bordering a bomb/flag, so check the memory for any -1
+            
+            
+GuessAr:    addi  $3, $0, -1     # (the input is for the command you want, -1 is Guess, 0 is to open)
+            j RightCen
+
+
+OpenAr:     addi $3, $0, 0  #0 for open
+
+
+
+RightCen:   addi $2, $2, 1
             mult $9, $2          #get the proper index 
             mflo $10
             lw $4, location($10) # check if box opened previously
-            beq $4, $0, OpenCell #open box if not opened already  
-            beq $4, $0, OpenAr #This is a zero, open all around
+            bne $4, $0, LeftCen #open box if not opened already
+                    #If it has already been open, then you leave it alone   
 
 
         #this is called when the cell being called to isn't open 
-OpenCell: addi $4, $4, 0 
+OpenCell: swi 568
           sw $4, location($10) # store box value   
-          swi 568
-          beq $4, $0, OpenAr  #count the number of bombs around first then call this function 
+          beq $4, $0, OpenAr  #count the number of bombs around first then call this function
 
 
 
 
-LeftCen:    sub $2, $2, $8
-            swi 568
+LeftCen:    addi $2, $2, -2
             mult $9, $2         #start open 
             mflo $10
             lw $4, location($10) # check if box opened previously
-            beq $4, $0, OpenCe1 #open box if not opened already  
-            swi 568
-            beq $4, $0, OpenAr #This is a zero, open all around
+            bne $4, $0, LeftTop #open box if not opened already 
 
 
         #this is called when the cell being called to isn't open 
-OpenCe1:  addi $4, $4, 0 
-          sw $4, location($10) # store box value   
+OpenCe1:  swi 568
+          sw $4, location($10) # store box value
+          beq $4, $0, OpenAr   
 
 
 
             #upper bound      
-LeftTop:    addi $8, $0, 8    #add 8 to each index to get the next row       
-            sub $2, $2, $8
-            swi 568
+LeftTop:    addi $2, $2, -8
             mult $9, $2         #start open 
             mflo $10
             lw $4, location($10) # check if box opened previously
-            beq $4, $0, OpenCe2 #open box if not opened already  
-            swi 568
-            beq $4, $0, OpenAr #This is a zero, open all around
+            bne $4, $0, CenterTop #open box if not opened already  
 
 
         #this is called when the cell being called to isn't open 
-OpenCe2:  addi $4, $4, 0 
+OpenCe2:  swi 568
           sw $4, location($10) # store box value   
+          beq $4, $0, OpenAr
           
 
 
 CenterTop:  addi $2,$2,1
-            swi 568
             mult $9, $2         #start open 
             mflo $10
             lw $4, location($10) # check if box opened previously
-            beq $4, $0, OpenCe3 #open box if not opened already  
-            swi 568
-            beq $4, $0, OpenAr #This is a zero, open all around
+            bne $4, $0, RightTop #open box if not opened already  
 
 
         #this is called when the cell being called to isn't open 
-OpenCe3:  addi $4, $4, 0 
+OpenCe3:  swi 568
           sw $4, location($10) # store box value   
+          beq $4, $0, OpenAr 
           
 
 
 RightTop:   addi $2,$2, 1
-            swi 568
             mult $9, $2         #start open 
             mflo $10
             lw $4, location($10) # check if box opened previously
-            beq $4, $0, OpenCe4 #open box if not opened already  
-            swi 568
-            beq $4, $0, OpenAr #This is a zero, open all around
+            bne $4, $0, BottomLeft #open box if not opened already  
 
 
         #this is called when the cell being called to isn't open 
-OpenCe4:  addi $4, $4, 0 
+OpenCe4:  swi 568
           sw $4, location($10) # store box value   
+          beq $4, $0, OpenAr
           
 
             #get lower bound
-BottomLeft: addi $8, $8, 6   #starts from the first column in the window
-            add $2,$2, $8
-            swi 568
+BottomLeft: addi $2,$2, 14
             mult $9, $2         #start open 
             mflo $10
             lw $4, location($10) # check if box opened previously
-            beq $4, $0, OpenCe5 #open box if not opened already  
-            swi 568
-            beq $4, $0, OpenAr #This is a zero, open all around
+            bne $4, $0, BottomCen #open box if not opened already 
 
 
         #this is called when the cell being called to isn't open 
-OpenCe5:  addi $4, $4, 0 
+OpenCe5:  swi 568
           sw $4, location($10) # store box value   
+          beq $4, $0, OpenAr 
           
 
 
 BottomCen:  addi $2,$2,1
-            swi 568
             mult $9, $2         #start open 
             mflo $10
             lw $4, location($10) # check if box opened previously
-            beq $4, $0, OpenCe6 #open box if not opened already  
-            swi 568
-            beq $4, $0, OpenAr #This is a zero, open all around
+            bne $4, $0, BottomRig #open box if not opened already  
 
 
         #this is called when the cell being called to isn't open 
-OpenCe6:  addi $4, $4, 0 
+OpenCe6:  swi 568
           sw $4, location($10) # store box value   
+          beq $4, $0, OpenAr 
           
 
 
 
 BottomRig:  addi $2, $2, 1
-            swi 568
             mult $9, $2         #start open 
             mflo $10
             lw $4, location($10) # check if box opened previously
-            beq $4, $0, OpenCe7 #open box if not opened already  
-            swi 568
-            beq $4, $0, OpenAr #This is a zero, open all around
+            bne $4, $0, END #open box if not opened already  
 
 
         #this is called when the cell being called to isn't open 
-OpenCe7:  addi $4, $4, 0 
+OpenCe7:  swi 568
           sw $4, location($10) # store box value   
+          beq $4, $0, OpenAr 
           
 
             j END
-
-
-
-            #check if open already 
-
-#Open:       ##responsible for opening the cell, but checking if it has been open first. If it has been open already, then check 
-            ##if it is a flag/bomb. If it hasn't been open, then open(call method) it and store the value to $10, if it already ##open, then store its value to $10
-Store:      addi $9, $0, 4
-            mult $2, $9
-            mflo $9
-            sw $2, location($9)
-
-check:      lw $4, location($10) #start check 
-
-OpenAr:     addi $3, $0, 0
-            j GuessAr
-
-
-
-
 
 END:  jr $31
